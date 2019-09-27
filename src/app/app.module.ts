844ef581bbb8d7be6564145bd6a52f76aea64440
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-27 15:01:53 -0500

    added home/invoice components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InvoiceComponent
   ],
   imports: [
     BrowserModule,
