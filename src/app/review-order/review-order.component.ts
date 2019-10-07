eee7a23a5719f775715aa1aee8427fb60a1d96e6
taylorm <tlaxin237@gmail.com>
2019-10-06 21:22:22 -0500

    set up th ets part of the review order

--- src/app/review-order/review-order.component.ts
+++ src/app/review-order/review-order.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // implement check boxes to select which orders to sebd, need icons
+
+export interface OrderElement {
+  productName: string;
+  subCategory: string;
+  volume: string;
+  quantity: number;
+  price: number;
+}
+
+const ORDER_DATA: OrderElement [] = [
+  {productName: 'Jack Daniels', subCategory: 'Bourbon', volume: '750mL', quantity: 4, price: 57.23},
+  {productName: 'Bulleit', subCategory: 'Bourbon', volume: '1L', quantity: 4, price: 52.74},
+  {productName: 'Eagle Rare', subCategory: 'Bourbon', volume: '750mL', quantity: 2, price: 36.14},
+  {productName: 'Jim Beam', subCategory: 'Bourbon', volume: '1L', quantity: 3, price: 33.77},
+  {productName: 'Old Forester', subCategory: 'Bourbon', volume: '750mL', quantity: 5, price: 48.29},
+  {productName: 'Blantons\'s', subCategory: 'Bourbon', volume: '750mL', quantity: 2, price: 47.10},
+];
+
 @Component({
   selector: 'app-review-order',
   templateUrl: './review-order.component.html',
@@ -13,6 +31,9 @@ export class ReviewOrderComponent implements OnInit {
   ) { }
 
   show = false;
+
+  displayedColumns: string[] = ['productName', 'subCategory', 'volume', 'quantity', 'price'];
+  dataSource = ORDER_DATA; // new MatTableDataSource<OrderElement>();
 
   ngOnInit() {
   }
