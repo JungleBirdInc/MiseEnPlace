cd4a2df1af51c181c49698f9a7038eb99ebef36b
taylorm <tlaxin237@gmail.com>
2019-10-08 16:51:04 -0500

    finalized edit invoice, invoice history in now complete as well, just need a submit button for when the search parameters have been set

--- src/app/edit-invoice/edit-invoice.component.ts
+++ src/app/edit-invoice/edit-invoice.component.ts
@@ -2,15 +2,7 @@ import { Component, OnInit, HostListener  } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
 
-export interface Inventory {
-  productName: string;
-  subCategory: string;
-  volume: string;
-  quantity: number;
-  price: number;
-}
-
-const REP1_DATA: Inventory [] = [
+const REP1_DATA = [
   {productName: 'Jack Daniels', subCategory: 'Bourbon', volume: '750mL', quantity: 4, price: 57.23},
   {productName: 'Bulleit', subCategory: 'Bourbon', volume: '1L', quantity: 4, price: 52.74},
   {productName: 'Eagle Rare', subCategory: 'Bourbon', volume: '750mL', quantity: 2, price: 36.14},
@@ -29,19 +21,8 @@ export class EditInvoiceComponent implements OnInit {
 constructor(
     public snackBar: MatSnackBar,
     private router: Router,
-    ) {
-     this.rep1 = [
-        {productName: 'Jack Daniels', subCategory: 'Bourbon', volume: '750mL', quantity: 4, price: 57.23},
-        {productName: 'Bulleit', subCategory: 'Bourbon', volume: '1L', quantity: 4, price: 52.74},
-        {productName: 'Eagle Rare', subCategory: 'Bourbon', volume: '750mL', quantity: 2, price: 36.14},
-        {productName: 'Jim Beam', subCategory: 'Bourbon', volume: '1L', quantity: 3, price: 33.77},
-        {productName: 'Old Forester', subCategory: 'Bourbon', volume: '750mL', quantity: 5, price: 48.29},
-        {productName: 'Blantons\'s', subCategory: 'Bourbon', volume: '750mL', quantity: 2, price: 47.10},
-      ];
-    }
+    ) {}
 
-  show = true;
-  displayedColumns: string[] = ['productName', 'subCategory', 'volume', 'quantity', 'price'];
   rep1 = REP1_DATA;
 
   ngOnInit() {}
@@ -53,9 +34,5 @@ constructor(
 
   finalizeInvoice() {
     this.router.navigate(['invoices']);
-  }
-
-  toggleShow() {
-    this.show = !this.show;
   }
 }
