34b1910460bf4b5af292b20e8350facb845da81b
taylorm <tlaxin237@gmail.com>
2019-10-12 14:42:19 -0500

    added update open bottles service

---
+++ src/app/services/updateOpenBottles.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class GetOpenBottlesService {
+
+    constructor(private http: HttpClient) {}
+
+    orgId = 1;
+
+    private _url: string = `/openBottles/newWeights/${this.orgId}`;
+
+    updateOpenBottles(bottles) {
+        const promise = new Promise((resolve, reject) => {
+            this.http.put(this._url, bottles)
+              .toPromise()
+              .then(
+                res => {
+                  resolve(res);
+                },
+                msg => {
+                  reject(msg);
+                }
+              );
+          });
+        return promise;
+    }
+}
