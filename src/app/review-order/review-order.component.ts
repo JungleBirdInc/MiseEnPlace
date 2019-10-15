57bd52a46a2afcf3c4f568d01146f7e89233057c
taylorm <tlaxin237@gmail.com>
2019-10-15 11:58:58 -0500

    created service to post orders, review orders dynamically renders

--- src/app/review-order/review-order.component.ts
+++ src/app/review-order/review-order.component.ts
@@ -1,24 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// implement check boxes to select which orders to sebd, need icons
+import { PostOrderService } from '../services/placeorder.service';
 
-export interface OrderElement {
-  productName: string;
-  unitCost: number;
-  volume: string;
-  quantity: number;
-  par: number;
-  ordered: number;
-}
-
-const REP1_DATA: OrderElement [] = [
-  {productName: 'Jack Daniels', unitCost: 10.17, volume: '750mL', quantity: 4, par: 5, ordered: 1},
-  {productName: 'Bulleit', unitCost: 10.17, volume: '1L', quantity: 4, par: 5, ordered: 1},
-  {productName: 'Eagle Rare', unitCost: 10.17, volume: '750mL', quantity: 2, par: 4, ordered: 2},
-  {productName: 'Jim Beam', unitCost: 10.17, volume: '1L', quantity: 3, par: 4, ordered: 2},
-  {productName: 'Old Forester', unitCost: 10.17, volume: '750mL', quantity: 5, par: 3, ordered: 0},
-  {productName: 'Blantons\'s', unitCost: 10.17, volume: '750mL', quantity: 2, par: 4, ordered: 2},
-];
 
 @Component({
   selector: 'app-review-order',
@@ -32,11 +15,13 @@ export class ReviewOrderComponent implements OnInit {
   ) { }
 
   show = false;
+  order = window.history.state.order;
+  public _placeOrder = PostOrderService;
 
   displayedColumns: string[] = ['productName', 'subCategory', 'volume', 'quantity', 'price'];
-  rep1 = REP1_DATA;
 
   ngOnInit() {
+    console.log(window.history.state.order);
   }
 
   sendOrders() {
@@ -53,6 +38,6 @@ export class ReviewOrderComponent implements OnInit {
   }
 
   confirmOrders() {
-    this.router.navigate(['orders']);
+    this.router.navigateByUrl('orders')
   }
 }
---
+++ src/app/services/placeorder.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostOrderService {
+
+  constructor(private http: HttpClient) { }
+
+  private _url: string = `order/send`;
+
+  sendOrder(data) {
+    let promise = new Promise((resolve, reject) => {
+      this.http.post(this._url, data)
+        .toPromise()
+        .then(
+          res => {
+            resolve(res);
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
+}
