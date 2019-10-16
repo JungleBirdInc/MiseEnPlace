0794346e420d38b4bc361907570c3b6649e1f501
taylorm <tlaxin237@gmail.com>
2019-10-15 19:58:07 -0500

    added functions to send orders to DB, need to calculate total price

--- src/app/review-order/review-order.component.ts
+++ src/app/review-order/review-order.component.ts
@@ -14,21 +14,68 @@ export class ReviewOrderComponent implements OnInit {
     private router: Router
   ) { }
 
-  show = false;
+  
   ordered = window.history.state.ordered;
   public _placeOrder: PostOrderService;
+  show = false;
+  show1 = false;
+  show2 = false;
+  show3 = false;
+  show4 = false;
 
   ngOnInit() {
   }
 
   toggleShow() {
     this.show = !this.show;
+    this.show1 = !this.show1;
+    this.show2 = !this.show2;
+    this.show3 = !this.show3;
+    this.show4 = !this.show4;
   }
 
-  confirmOrders(data) {
-    this._placeOrder.sendOrder(data)
-    .then(() => {
-      this.router.navigateByUrl('orders');
-    });
+  confirmOrders1(data) {
+    const send = {
+      admin_id: 1,
+      type: 3,
+      rep_id: data[0].rep_id,
+      total_price: 'need to fix',
+      weeklySet: data
+    };
+    this._placeOrder.sendOrder(send);
+    this.show1 = !this.show1;
+  }
+  confirmOrders2(data) {
+    const send = {
+      admin_id: 1,
+      type: 3,
+      rep_id: data[0].rep_id,
+      total_price: 'need to fix',
+      weeklySet: data
+    };
+    this._placeOrder.sendOrder(send);
+    this.show2 = !this.show2;
+  }
+  confirmOrders3(data) {
+    const send = {
+      admin_id: 1,
+      type: 3,
+      rep_id: data[0].rep_id,
+      total_price: 'need to fix',
+      weeklySet: data
+    };
+    this._placeOrder.sendOrder(send);
+    this.show3 = !this.show3;
+  }
+  confirmOrders4(data) {
+    const send = {
+      admin_id: 1,
+      type: 3,
+      rep_id: data[0].rep_id,
+      total_price: 'need to fix',
+      weeklySet: data
+    };
+    this._placeOrder.sendOrder(send);
+    this.show4 = !this.show4;
   }
 }
