d6f057c46b3910e216f51778e7c9cec8e678430e
Nicholas Kolodziej <kolonick@gmail.com>
2019-10-19 11:29:09 -0500

    minor scale styling

--- src/app/scale/scale.component.ts
+++ src/app/scale/scale.component.ts
@@ -12,16 +12,16 @@ import { GetweightService } from '../services/getweight.service';
   template:
   `<h3 class='titles'>Weigh Bottle</h3><br />
 
-<div class='container'>
-  <h4 *ngIf='weight === undefined' class="smlTitle">PLACE BOTTLE ON SCALE</h4>
-  <h4 *ngIf='weight' class="smlTitle">{{weight.weight}} ounces</h4><br />
+  <div class='container'>
+  <h5 *ngIf='weight === undefined' class="smlTitle">< Place bottle on scale ></h5>
+  <h5 *ngIf='weight' class="smlTitle">{{weight.weight}} ounces</h5><br />
 
-  PRODUCT: {{targetBottle.distributors_product.product.product_name}} <br />
-  UPC: {{code}} <br />
-  WEIGHT:  <input type="text" value={{this.weight.weight}} contenteditable="true" />
+  <p class='category'>Product:</p> {{targetBottle.distributors_product.product.product_name}} <br />
+  <p class='category'>UPC: {{code}} <br />
+  <p class='category'>Weight:  <input type="text" value={{this.weight.weight}} contenteditable="true" />
   <button *ngIf='this.weight.weight !== 0' (click)='weighAgain()'>*Weigh again</button> <br />
-  PRV. WEIGHT: {{(targetBottle.weight / 100).toFixed(2)}} <br />
-  BASE TARE: {{(targetBottle.distributors_product.product.tare / 100).toFixed(2)}}<br />
+  <p class='category'>Prev. Weight: {{(targetBottle.weight / 100).toFixed(2)}} <br />
+  <p class='category'>Tare: {{(targetBottle.distributors_product.product.tare / 100).toFixed(2)}}<br />
 
   <button routerLink='../scan-bar-code'>Scan Another Bottle</button> 
   <button routerLink='../burn-list'>Finish Weighing</button>`
