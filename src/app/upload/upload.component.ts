e59e60b895924a46fc1435f8452bd435e1382c2c
taylorm <tlaxin237@gmail.com>
2019-10-21 13:38:41 -0500

    fixed tsling erors

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -15,7 +15,7 @@ export class UploadComponent implements OnInit {
 
   constructor(
     public router: Router,
-    public dialog: MatDialog, 
+    public dialog: MatDialog,
     public uploadService: UploadService,
     private _getbotsize: GetbotsizeService,
     private _newinvoice: NewinvoiceService,
@@ -27,8 +27,8 @@ export class UploadComponent implements OnInit {
 
   ngOnInit() {
 
-    this.openUploadDialog()
-  
+    this.openUploadDialog();
+
     this._getbotsize.getCategories()
       .then(data => {
         this.botsize = data;
@@ -36,23 +36,23 @@ export class UploadComponent implements OnInit {
       });
   }
 
-  import(){
+  import() {
     // console.log(window.localStorage.getItem('invoice'));
     this.invoiceRaw = JSON.parse(window.localStorage.getItem('invoice'));
     console.log('INVOICE RAW', this.invoiceRaw);
 
-      const prod = {
-        invoice: this.invoiceRaw.body.invoice || null,
-        distributor: this.invoiceRaw.body.matches[0].name || null,
-        dist_id: undefined,
-        rep_id: 1, //check
-        receiptSet: [],
-        total_price: 0,
-        url: null,
-        admin_id: 1 //default (change with auth)
-      }
+    const prod = {
+    invoice: this.invoiceRaw.body.invoice || null,
+    distributor: this.invoiceRaw.body.matches[0].name || null,
+    dist_id: undefined,
+    rep_id: 1, // check
+    receiptSet: [],
+    total_price: 0,
+    url: null,
+    admin_id: 1 // default (change with auth)
+  };
 
-      this.invoiceRaw.body.products.forEach(product => {
+    this.invoiceRaw.body.products.forEach(product => {
         if (product.guesses.length !== 0) {
 
           product.guesses.forEach(guess => {
@@ -72,64 +72,64 @@ export class UploadComponent implements OnInit {
             };
 
             this.botsize.forEach(bottle => {
-                if(indProd.btlSizeId === bottle.id){
+                if (indProd.btlSizeId === bottle.id) {
                   indProd.bottleSize = bottle.size;
                 }
             });
 
             let count = true;
-            for(let i = 0; i < prod.receiptSet.length; i++){
-              if(prod.receiptSet[i].name === indProd.name){
+            for (let i = 0; i < prod.receiptSet.length; i++) {
+              if (prod.receiptSet[i].name === indProd.name) {
                 count = false;
               }
             }
 
-            if(count){
+            if (count) {
               prod.receiptSet.push(indProd);
             }
 
             count = true;
 
-          });//for each guess
-        }//if statement
-      });//for each product
+          }); // for each guess
+        }// if statement
+      }); // for each product
 
-      this.invoice = prod;
-  }//import
+    this.invoice = prod;
+  } // import
 
-  saveData(){
+  saveData() {
     console.log('saved!');
-    localStorage.removeItem('invoice')
+    localStorage.removeItem('invoice');
 
     this._newinvoice.newInvoice(this.invoice)
       .then(data => {
         console.log('newinvoice return', data);
-        return this.router.navigate(['invoices'])
+        return this.router.navigate(['invoices']);
       });
   }
 
-  calcTotal(){
+  calcTotal() {
     console.log(this.invoice);
 
-    if(this.invoice !== undefined){
+    if (this.invoice !== undefined) {
       let sum = 0;
       this.invoice.receiptSet.forEach(product => {
         sum += product.qty * product.price;
       });
       this.invoice.total_price = sum;
       return sum;
-    }else{
+    } else {
       return 0;
     }
   }
-  
-  remove(i: string | number){
+
+  remove(i: string | number) {
     console.log('deleted', this.invoice.receiptSet[i]);
     this.invoice.receiptSet.splice(i, 1);
     console.log(this.invoice.receiptSet);
   }
 
   public openUploadDialog() {
-    let dialogRef = this.dialog.open(DialogComponent);  
+    let dialogRef = this.dialog.open(DialogComponent);
   }
-} 
+}
