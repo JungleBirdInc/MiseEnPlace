de268b332b830bac9bec91404d2f4662a433e214
Nicholas Kolodziej <kolonick@gmail.com>
2019-10-24 14:52:29 -0500

    added url to get bottle by upc service

--- src/app/services/getbottlebyupc.service.ts
+++ src/app/services/getbottlebyupc.service.ts
@@ -14,7 +14,7 @@ export class GetBottleByUPCService {
   getBottleUPC(upc) {
     console.log(`UPC: ${upc}`);
     let promise = new Promise((resolve, reject) => {
-      this.http.get(`/openBottles/getAll/1`)
+      this.http.get(`${environment.baseURL}/openBottles/getAll/1`)
         .toPromise()
         .then(
           res => {
